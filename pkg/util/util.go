d8c6edc19b398c1572d1d8f1a6fcca3750b24ec8
suzi1037 <xiaoxin09@hotmail.com>
2025-05-19 09:52:23 +0800

    ParseTpl
    Signed-off-by: suzi1037 <xiaoxin09@hotmail.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,11 @@
 package util
 
 import (
+	"bytes"
 	"path/filepath"
+	"text/template"
+
+	"github.com/pkg/errors"
 )
 
 func GetAbsolutePath(path string) string {
@@ -10,4 +14,42 @@ func GetAbsolutePath(path string) string {
 	}
 	path, _ = filepath.Abs(path)
 	return path
+}
+
+func ParseTplStringWithDelims(tplt string, data any, left, right string) (string, error) {
+	var res string
+	tpl, err := template.New("").Delims(left, right).Parse(tplt)
+	if err != nil {
+		return res, errors.Wrap(err, "parse template")
+	}
+	var buf bytes.Buffer
+
+	err = tpl.Execute(&buf, data)
+	if err != nil {
+		return res, errors.Wrap(err, "tpl execute")
+	}
+	return buf.String(), nil
+}
+
+func ParseTplString(tplt string, data any) (string, error) {
+	return ParseTplStringWithDelims(tplt, data, "{{", "}}")
+}
+
+func ParseTplBytesWithDelims(tplt []byte, data any, left, right string) ([]byte, error) {
+	tpl, err := template.New("").Delims(left, right).Parse(string(tplt))
+	// Delims
+	if err != nil {
+		return nil, errors.Wrap(err, "parse template")
+	}
+	var buf bytes.Buffer
+
+	err = tpl.Execute(&buf, data)
+	if err != nil {
+		return nil, errors.Wrap(err, "tpl execute")
+	}
+	return buf.Bytes(), nil
+}
+
+func ParseTplBytes(tplt []byte, data any) ([]byte, error) {
+	return ParseTplBytesWithDelims(tplt, data, "{{", "}}")
 }
